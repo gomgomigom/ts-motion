64f98a3275c58fa329c8847625f81e1a593c912a
gomgomigom <nodam89@naver.com>
2022-05-25 08:41:54 +0900

    css

--- try2/build/main.js
+++ try2/build/main.js
@@ -1,2 +1,13 @@
 "use strict";
-console.log('hello');
+{
+    const addModal = (e) => {
+        console.log(e.path);
+        console.log('haha');
+    };
+    const button = document.querySelectorAll('.btn');
+    if (button != null) {
+        console.log(button);
+        button.forEach((item) => console.log(item));
+        button.forEach(() => addEventListener('click', addModal));
+    }
+}
--- try2/src/main.ts
+++ try2/src/main.ts
@@ -1,1 +1,13 @@
-console.log('hello`');
+{
+  const addModal = (e) => {
+    console.log(e.path);
+    console.log('haha');
+  };
+
+  const button: NodeList | null = document.querySelectorAll('.btn');
+  if (button != null) {
+    console.log(button);
+    button.forEach((item) => console.log(item));
+    button.forEach(() => addEventListener('click', addModal));
+  }
+}
